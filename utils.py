c90a1f4ac21448070c7899139387990f427d6b66
Rebe-nya <filip.slabinaf@gmail.com>
2025-08-18 22:36:35 +0200

    close_window()

--- utils.py
+++ utils.py
@@ -1,7 +1,12 @@
 from libs import QUIT, pygame, display, time, set_mode
 
 def text_font(size):
-    return pygame.font.Font("Fonts/04b_30/04B_30__.ttf", size)
+    return pygame.font.Font("fonts/04b_30/04B_30__.ttf", size)
+
+def close_window():
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            quit()
 
 # Default values
 screenSize = (800, 800)
