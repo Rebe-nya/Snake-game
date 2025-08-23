521986e9afa83ee54fb6d6c99ce9611c3604c8aa
Rebe-nya <filip.slabinaf@gmail.com>
2025-08-23 03:32:01 +0200

    Better square render

--- gfx.py
+++ gfx.py
@@ -1,5 +1,5 @@
 from libs import *
-from utils import text_font, screenSize, screen, blit, close_window
+from utils import text_font, screenSize, screen, blit, close_window, center
 
 # Menu
 def menu_render(speed, play_rect, exit_rect, header_font, header_rect, menu_font, play, exit, button_offset):
@@ -9,16 +9,17 @@ def menu_render(speed, play_rect, exit_rect, header_font, header_rect, menu_font
             blit(header, header_rect)
             blit(play, play_rect)
             blit(exit, exit_rect)
-
         def select(x, y):
             select = menu_font.render(">", True, (255, 255, 255))
             select_rect = select.get_rect(center=center(-x - 32, y))
             blit(select, select_rect)
-
+        
+        # Rainbow header
         rgb = colorsys.hsv_to_rgb(i / 360, 1, 1)
         color = tuple(int(c * 255) for c in rgb)
         header = header_font.render("Snake Game", True, color)
 
+        # Selection symbol, cursor
         if play_rect.collidepoint(get_pos()):
             text_render()
             select(play_rect[3], 0)
@@ -30,11 +31,12 @@ def menu_render(speed, play_rect, exit_rect, header_font, header_rect, menu_font
         else:
             text_render()
             flip()
-
         if play_rect.collidepoint(get_pos()) or exit_rect.collidepoint(get_pos()):
             set_cursor(SYSTEM_CURSOR_HAND)
         else:
             set_cursor(SYSTEM_CURSOR_ARROW)
+        
+        # Menu button functions
         for event in pygame.event.get():
             if event.type == QUIT:
                 quit()
@@ -54,11 +56,6 @@ def menu_render(speed, play_rect, exit_rect, header_font, header_rect, menu_font
                     quit()
         time.wait(speed)
     return True
-
-def center(x, y):
-    x += screenSize[0] // 2
-    y += screenSize[1] // 2
-    return (x, y)
 
 def menu():
     header_size = 40
@@ -88,35 +85,7 @@ def menu():
         pass
 
 # Game Map
-def map():
-    screen.fill((16, 16, 16))
-    offset = 40
-    sqr = 40
-    for opacity in range(0, 256, 8):
-        surface = pygame.Surface((screenSize[0], screenSize[1]), pygame.SRCALPHA)
-        frame_height = frame(opacity, surface)
-        lightGreen = (121, 198, 83, opacity)
-        darkGreen = (106, 191, 64, opacity)
-        num_x_sqr = 0
-        num_y_sqr = 0
-        for j in range(((screenSize[1] - offset - frame_height) // sqr) - offset // sqr):
-            num_y_sqr += 1
-            for i in range(((screenSize[0] - offset) // sqr) - offset // sqr):
-                num_x_sqr += 1
-                x = i * sqr + offset
-                y = j * sqr + frame_height + offset
-                if (i + j) % 2 == 0:
-                    rect(surface, lightGreen, (x, y, sqr, sqr))
-                else:
-                    rect(surface, darkGreen, (x, y, sqr, sqr))
-                close_window()
-        screen.blit(surface, (0, 0))
-        flip()
-        time.wait(5)
-    num_x_sqr = num_x_sqr // num_y_sqr
-    return num_x_sqr, num_y_sqr
-    
-def frame(opacity, surface):
+def top_bar(opacity, surface, top_bar_height):
     def icons():
         apple = image.load("assets/apple.png").convert_alpha()
         highscore = image.load("assets/highscore.png").convert_alpha()
@@ -127,17 +96,44 @@ def frame(opacity, surface):
         apple.set_alpha(opacity)
         highscore.set_alpha(opacity)
 
-        apple_rect = apple.get_rect(center=(height // 2, height // 2))
-        highscore_rect = highscore.get_rect(center=(height // 2 + 125, height // 2))
+        apple_rect = apple.get_rect(center=(top_bar_height // 2, top_bar_height // 2))
+        highscore_rect = highscore.get_rect(center=(top_bar_height // 2 + 125, top_bar_height // 2))
 
         surface.blit(apple, apple_rect)
         surface.blit(highscore, highscore_rect)
 
-    height = 80
     surface.fill((74, 134, 45, opacity))
-    rect(surface, (64, 115, 38, opacity), (0, 0, screenSize[0], height))
+    rect(surface, (64, 115, 38, opacity), (0, 0, screenSize[0], top_bar_height))
     icons()
-    return height
 
+def map():
+    top_bar_height = 80
 
+    num_x_sqr = 10
+    num_y_sqr = 10
+    sqr_size = 60
+    center_x, center_y = center(0, 0)
 
+    for opacity in range(0, 256, 8):
+        surface = pygame.Surface((screenSize[0], screenSize[1]), pygame.SRCALPHA)
+        top_bar(opacity, surface, top_bar_height)
+
+        lightGreen = (121, 198, 83, opacity)
+        darkGreen = (106, 191, 64, opacity)
+        x = 0
+        y = 0
+
+        for j in range(num_y_sqr):
+            for i in range(num_x_sqr):
+                x = i * sqr_size + center_x - (num_x_sqr * sqr_size) // 2
+                y = j * sqr_size + center_y - (num_y_sqr * sqr_size) // 2 + top_bar_height // 2
+                if (i + j) % 2 == 0:
+                    rect(surface, lightGreen, (x, y, sqr_size, sqr_size))
+                else:
+                    rect(surface, darkGreen, (x, y, sqr_size, sqr_size))
+                close_window()
+
+        screen.blit(surface, (0, 0))
+        flip()
+        time.wait(5)
+    return num_x_sqr, num_y_sqr
