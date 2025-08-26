4c463e1ec0804c619b717aadb6dac9a19da1e44f
Rebe-nya <filip.slabinaf@gmail.com>
2025-08-27 01:42:01 +0200

    Better function name's, game grid

--- snake.py
+++ snake.py
@@ -1,22 +1,26 @@
-from libs import *
+from libs import filled_circle, aacircle, filled_polygon, aapolygon
 from utils import screen, close_window
-from gfx import map
+from gfx import map_x, map_y, sqr_size
 
-def circle_render(x, y, size):
-    size = (sqr_size // 2) * (size / 100)
+color = (0, 0, 255)
+
+def game_grid(x, y):
     x = map_x + sqr_size / 2 + x * sqr_size
     y = map_y + sqr_size / 2 + y * sqr_size
-    #print("Map:", map_x, map_y, sqr_size)
-    #print("Circle:", x, y, size * 2)
+    return x, y
+
+def render_circle(x, y, size):
+    size = (sqr_size / 2) * (size / 100)
+    x, y = game_grid(x, y)
     size = round(size)
     filled_circle(screen, round(x), round(y), size, color)
     aacircle(screen, round(x), round(y), size, color)
     return size
 
-def polygon_render(x1, y1, size1, x2, y2, size2):
+def render_polygon(x1, y1, size1, x2, y2, size2):
     def position(x, y, size):
-        x = map_x + sqr_size // 2 + x * sqr_size
-        y = map_y + sqr_size // 2 - size + y * sqr_size
+        x, y = game_grid(x, y)
+        y = y - size
         return x, y
     x1, y1 = position(x1, y1, size1)
     x2, y2 = position(x2, y2, size2)
@@ -24,13 +28,15 @@ def polygon_render(x1, y1, size1, x2, y2, size2):
     filled_polygon(screen, points, color)
     aapolygon(screen, points, color)
 
-map_x, map_y, num_x_sqr, num_y_sqr, sqr_size = map()
-color = (0, 0, 255)
-
-size1 = circle_render(0, 0, 80)
-size2 = circle_render(9, 0, 40)
-polygon_render(0, 0, size1, 9, 0, size2)
+"""
+size1 = render_circle(0, 0, 80)
+size2 = render_circle(9, 0, 40)
+render_polygon(0, 0, size1, 9, 0, size2)
 flip()
+
+while True:
+    close_window()
+"""
 
 '''
 def circle_body(x1, y1, size1, x2, y2, size2):
@@ -47,6 +53,3 @@ for position in range(6):
     arc(screen, 50 - position, 50 - position, 5 - position, 180, 270, (0, 0, 255))
 flip()
 '''
-
-while True:
-    close_window()
