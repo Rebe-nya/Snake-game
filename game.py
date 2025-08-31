3c11ff95c642c133de07c7f14a6fc1d0de274d99
Rebe-nya <filip.slabinaf@gmail.com>
2025-08-31 02:03:34 +0200

    Fixed bad merge

--- game.py
+++ game.py
@@ -27,26 +27,6 @@ def game():
         if (x, y) != history[-1]: history.append((x, y))
         if len(history) > body_num: history.pop(0)
         
-    global x, y, direction
-    x, y = 1.0, 4.0
-    speed = 1
-    history = [(x - 1, y), (x, y), (x + 1, y)]
-    direction = None
-
-    def move_snake(body_num):
-        global x, y, direction
-
-        if direction != None:
-            if direction == "left" and x > 0: x -= 1
-            elif direction == "right" and x < num_sqr[0] - 1: x += 1
-            elif direction == "up" and y > 0: y -= 1
-            elif direction == "down" and y < num_sqr[1] - 1: y += 1
-            history.append((x, y))
-            history.pop(0)
-
-        if (x, y) != history[-1]: history.append((x, y))
-        if len(history) > body_num: history.pop(0)
-        
         map()
         for i in range(body_num):
             x1, y1 = history[i]
@@ -58,22 +38,14 @@ def game():
 
     running = True
     while running:
-    running = True
-    while running:
         for event in pygame.event.get():
             if event.type == QUIT:
                 running = False
                 running = False
 
-        print(history)
-
         clock.tick(fps)
 
-        for i in range(speed * 100):
-            keys = key.get_pressed()
         print(history)
-
-        clock.tick(fps)
 
         for i in range(speed * 100):
             keys = key.get_pressed()
